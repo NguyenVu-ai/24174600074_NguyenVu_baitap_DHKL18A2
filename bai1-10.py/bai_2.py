caa648784d90985cf9380f339be6324ae2624f57
minhtu <ptlinh.24174600106@sv.uneti.edu.vn>
2024-10-31 16:21:28 +0700

    bai_2

---
+++ bai1-10.py/bai_2.py
@@ -0,0 +1,13 @@
+x = float(input("nhập tọa độ x của điểm M:"))
+y = float(input("nhập tọa độ y của điểm M:"))
+a = float(input("nhập tọa độ a của diểm I:"))
+b = float(input("nhập tọa độ b của diểm I:"))
+r = float(input("nhập bán kính R:"))
+m_i = (x-a)**2+(y-b)**2
+ban_kinh = r**2
+if m_i <= ban_kinh:
+    print("nằm trong đường tròn")
+    print("đúng")
+else:
+    print("không nằm trong dường tròn")
+    print("sai")
